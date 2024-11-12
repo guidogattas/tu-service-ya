b3163e02baa30d3e042cc2ecc62626fa089554ba
Guido Gattas <guido.gattas@gmail.com>
2024-11-12 20:16:55 -0300

    Whatsapp number fix

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -6,17 +6,26 @@ const WhatsApp = () => {
     const message = 'Buenos días, quisiera consultarte por...';
     const encodedMessage = encodeURIComponent(message);
 
-
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
     const link = isMobile
         ? `whatsapp://send?phone=${phoneNumber}&text=${encodedMessage}`
         : `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
 
     return (
         <div className='flex items-center p-2 mr-6 space-x-2'>
-            <a href={link} target="_blank" rel="noopener noreferrer">
-                <WhatsAppLogo height="70" fill='white' className="w-12 text-green-500 transition-colors duration-200 md:w-16 hover:text-green-700" />
+            <a
+                href={link}
+                rel="noopener noreferrer"
+                {...(isMobile ? {} : { target: "_blank" })}
+            >
+                <WhatsAppLogo
+                    height="70"
+                    fill='white'
+                    className="w-12 text-green-500 transition-colors duration-200 md:w-16 hover:text-green-700"
+                />
             </a>
+            <a href="https://api.whatsapp.com/send?phone=1234567890" target='_blank'>Contact Us on WhatsApp</a>
             <div className='flex flex-col'>
                 <p className='text-white font-xbold text-md md:text-xl font-inter'>{phoneNumberWA}</p>
                 <p className='text-sm text-white md:text-lg font-inter'>Contactanos por WhatsApp</p>
