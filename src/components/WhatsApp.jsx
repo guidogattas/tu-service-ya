fe7146476b149e14986177d491779142e7cc84bd
Guido Gattas <guido.gattas@gmail.com>
2024-11-12 20:01:04 -0300

    Whatsapp number fix

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -1,8 +1,9 @@
 import WhatsAppLogo from '../assets/icons/WhatsAppLogo.jsx';
 
 const WhatsApp = () => {
-    const phoneNumber = '11 3126 5226';
-    const encodedMessage = 'Buenos días, quisiera consultarte por...';
+    const phoneNumber = '5491131265226';
+    const phoneNumberWA = '1131265226';
+    const encodedMessage = encodeURIComponent('Buenos días, quisiera consultarte por...');
 
     return (
         <div className='flex items-center p-2 mr-6 space-x-2'>
@@ -10,7 +11,7 @@ const WhatsApp = () => {
                 <WhatsAppLogo height="70" fill='white' className="w-12 text-green-500 transition-colors duration-200 md:w-16 hover:text-green-700" />
             </a>
             <div className='flex flex-col'>
-                <p className='text-white font-xbold text- md:text-xl font-inter'>{phoneNumber}</p>
+                <p className='text-white font-xbold text-md md:text-xl font-inter'>{phoneNumberWA}</p>
                 <p className='text-sm text-white md:text-lg font-inter'>Contactanos por WhatsApp</p>
             </div>
         </div>
