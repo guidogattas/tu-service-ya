b12e4b1d39b8bcaa05dfa954a97d3f336bd5ab8f
Guido Gattas <guido.gattas@gmail.com>
2024-11-12 20:09:44 -0300

    Whatsapp number fix

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -2,12 +2,19 @@ import WhatsAppLogo from '../assets/icons/WhatsAppLogo.jsx';
 
 const WhatsApp = () => {
     const phoneNumber = '5491131265226';
-    const phoneNumberWA = '1131265226';
-    const encodedMessage = encodeURIComponent('Buenos días, quisiera consultarte por...');
+    const phoneNumberWA = '11 3126 5226';
+    const message = 'Buenos días, quisiera consultarte por...';
+    const encodedMessage = encodeURIComponent(message);
+
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const link = isMobile
+        ? `whatsapp://send?phone=${phoneNumber}&text=${encodedMessage}`
+        : `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
 
     return (
         <div className='flex items-center p-2 mr-6 space-x-2'>
-            <a href={`https://wa.me/${phoneNumber}?text=${encodedMessage}`} target="_blank" rel="noopener noreferrer">
+            <a href={link} target="_blank" rel="noopener noreferrer">
                 <WhatsAppLogo height="70" fill='white' className="w-12 text-green-500 transition-colors duration-200 md:w-16 hover:text-green-700" />
             </a>
             <div className='flex flex-col'>
